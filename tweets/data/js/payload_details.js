6f775a88f210a9d6a12790c27a41e823e0480e66
Kirtan Patel <me@thekirtan.com>
2014-04-12 23:53:10 -0400

    cool

--- tweets/data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details =  {
-  "tweets" : 5019,
-  "created_at" : "Tue Aug 13 22:08:27 +0000 2013",
-  "lang" : "en"
-}
--- tweets/data/js/user_details.js
+++
@@ -1,9 +0,0 @@
-var user_details =  {
-  "expanded_url" : "http://kirtan.me",
-  "screen_name" : "kirtan",
-  "url" : "http://t.co/sXEK4rCSgR",
-  "full_name" : "Kirtan Patel",
-  "id" : "14577999",
-  "created_at" : "Tue Apr 29 00:35:38 +0000 2008",
-  "display_url" : "kirtan.me"
-}
