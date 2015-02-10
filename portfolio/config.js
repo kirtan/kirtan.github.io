8d5c277a710210d51ac60a47ef4d054939f690db
Kirtan Patel <me@thekirtan.com>
2015-02-10 18:58:43 -0500

    added portfolio

---
+++ portfolio/config.js
@@ -0,0 +1,10 @@
+// Howdy! This is your website's configuration file.
+// Just replace my personal info with yours, save, and you're done :)
+
+dribbbox.config = {
+
+  dribbble_username : "kirtan",
+  short_description : "digital and print design",
+  email_address     : "me@thekirtan.com"
+
+}
